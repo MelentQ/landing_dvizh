d1d04915c2914f353ba06659c26dd90d45938415
MelentQ <mejleht@yandex.ru>
2021-09-02 18:57:25 +0300

    intro fixes

--- src/js/components/toggleStickyHeader.js
+++ src/js/components/toggleStickyHeader.js
@@ -1,25 +1,31 @@
 /**
- * Управляет логикой появления второго хедера после скролла интро блока
+ * Управляет логикой хедера
+ * Кастомный position: sticky
  */
 function toggleStickyHeader() {
   const stickyHeader = document.querySelector('.sticky-header');
   if (!stickyHeader) return;
 
   const introElement = document.querySelector('.intro');
+  const aboutElement = document.querySelector('.about');
 
   let isOpen = false;
 
   const toggle = () => {
     const introHeight = introElement.clientHeight;
+    const aboutHeight = aboutElement.clientHeight;
+    const stickyHeaderHeight = stickyHeader.clientHeight;
+    const togglePositon = introHeight + aboutHeight - stickyHeaderHeight;
+
     const scrollPos = window.pageYOffset;
 
-    if (scrollPos >= introHeight && !isOpen) {
-      stickyHeader.classList.add('sticky-header_opened');
+    if (scrollPos >= togglePositon && !isOpen) {
+      stickyHeader.classList.add('sticky-header_fixed');
       isOpen = true;
     }
     
-    if (scrollPos < introHeight) {
-      stickyHeader.classList.remove('sticky-header_opened');
+    if (scrollPos < togglePositon) {
+      stickyHeader.classList.remove('sticky-header_fixed');
       isOpen = false;
     }
   }
