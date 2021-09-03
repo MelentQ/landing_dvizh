26d629a98375085d426d32c0ac8f1d3aea4ded6f
elizavetaShved <eli3avetta.shvedchikova@gmail.com>
2021-09-03 17:32:02 +0300

    promise init slider desk

--- src/js/components/initPromisesSlider.js
+++ src/js/components/initPromisesSlider.js
@@ -1,18 +1,71 @@
-import Swiper from "swiper";
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
-/**
- * Включает слайдер на секции "Каким будет этот форум"
- */
+gsap.registerPlugin(ScrollTrigger);
+import imagesLoaded from 'imagesloaded';
+
 function initPromisesSlider() {
-  const container = document.querySelector('.promises__slider');
+  const hostElem = document.getElementById('promises-host');
+  if (!hostElem) return;
 
-  if (!container) return;
+  const listItemElems = Array.from(hostElem.querySelectorAll('.promises__list-item'));
+  if (listItemElems.length <= 1) return;
 
-  new Swiper('.promises__slider', {
-    slidesPerView: 1,
-    spaceBetween: 120,
-    mousewheel: true
-  })
+  const pageMain = document.querySelector('.page-main');
+
+  // создание обертки для секций
+  const createNewWrapperGsap = () => {
+    const newWrapperGsap = document.createElement('div');
+    newWrapperGsap.classList.add('new-wrapper-gsap');
+    const sectionsElems = document.querySelectorAll('section');
+
+    let isAchievingCurrentSection = false;
+    sectionsElems.forEach(sectionItem => {
+      if (!isAchievingCurrentSection) {
+        newWrapperGsap.appendChild(sectionItem);
+        isAchievingCurrentSection = sectionItem === hostElem;
+      }
+    })
+    pageMain.prepend(newWrapperGsap);
+  }
+
+  // инициализация gsap
+  const initGsap = () => {
+    const horizontalSections = gsap.utils.toArray('.gsap__slider-container')
+    horizontalSections.forEach(sec => {
+      const thisPinWrap = sec.querySelector('.gsap__wrapper');
+      const thisAnimWrap = thisPinWrap.querySelector('.gsap__list');
+
+      const getToValue = () => {
+        const margin = parseFloat(window.getComputedStyle(listItemElems[0]).getPropertyValue("margin-right"));
+        const totalElementsWidth = listItemElems.reduce((acc, currentElement) => acc + currentElement.offsetWidth, 0);
+        const totalWidth = (totalElementsWidth + margin * (listItemElems.length - 1)) * -1;
+
+        return totalWidth + listItemElems[0].offsetWidth;
+      }
+
+      gsap.to(thisAnimWrap, {
+        x: () => getToValue(),
+        ease: "none",
+        scrollTrigger: {
+          trigger: document.querySelector('.promises__container'),
+          start: "bottom bottom",
+          end: () => "+=" + thisAnimWrap.scrollWidth,
+          pin: document.querySelector('.new-wrapper-gsap'),
+          pinSpacing: true,
+          invalidateOnRefresh: true,
+          scrub: true
+        }
+      });
+    });
+  }
+
+  const imgLoad = imagesLoaded(pageMain);
+
+  imgLoad.on('always', () => {
+    createNewWrapperGsap();
+    initGsap();
+  });
 }
 
 export default initPromisesSlider;
